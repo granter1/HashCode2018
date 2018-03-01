adc5ad029e90f071195fec32560358471d008982
chloejolliffe <chloe.jolliffe@ucdconnect.ie>
2018-03-01 19:38:42 +0000

    Added roster class

--- ride.rb
+++ ride.rb
@@ -1,6 +1,6 @@
 class Ride
 
-  attr_reader :distance
+  attr_reader :distance, :time
 
   def initialize(rowStart,columnStart,rowFinish,columnFinish,earliestStart,latestFinish)
     @rowStart=rowStart
@@ -10,6 +10,7 @@ class Ride
     @distance=(rowStart-rowFinish).abs + (columnStart-columnFinish).abs
     @start=earliestStart
     @finish=latestFinish
+    @time = latestFinish - earliestStart
   end
 
 end
---
+++ roster.rb
@@ -0,0 +1,16 @@
+require_relative 'fleet'
+require_relative 'rides'
+
+class Roster
+
+  def initialize
+    @roster = {}
+  end
+
+  def allocate_rides(fleet, rides)
+    rides.each_with_index do |ride|
+      #if fleet.cars[0].distance + ride.distance < ride.time
+      @roster[ride] = fleet.vehicles[0]
+    end
+  end
+end
