865a1018f35d691521642256f45e898b90c110f7
chloejolliffe <chloe.jolliffe@ucdconnect.ie>
2018-03-01 19:40:15 +0000

    renamed rides array

--- rides.rb
+++ rides.rb
@@ -1,13 +1,13 @@
 require_relative 'ride.rb'
 
 class Rides
-  attr_reader :rides
+  attr_reader :ride_list
 
   def initialize(rides = nil)
-    @rides = rides.equal?(nil) ? [] : rides
+    @ride_list = rides.equal?(nil) ? [] : rides
   end
 
   def add_ride(ride)
-    @rides.push(ride)
+    @ride_list.push(ride)
   end
 end
--- roster.rb
+++ roster.rb
@@ -8,7 +8,7 @@ class Roster
   end
 
   def allocate_rides(fleet, rides)
-    rides.each_with_index do |ride|
+    rides.ride_list.each_with_index do |ride|
       #if fleet.cars[0].distance + ride.distance < ride.time
       @roster[ride] = fleet.vehicles[0]
     end
